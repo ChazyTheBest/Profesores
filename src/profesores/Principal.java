54ee522fdf6ef9e4f11ced9f320f832e344c3b81
Gabriel Blanca Montes <how2hack@hotmail.es>
2019-12-05 15:34:32 +0100

    Profesores

---
+++ src/profesores/Principal.java
@@ -0,0 +1,281 @@
+package profesores;
+
+import java.util.Scanner;
+
+public class Principal
+{
+    private static Profesor[] altaProfesor(Profesor[] lista, Profesor p)
+    {
+/*
+ * También se puede hacer de esta manera (sustituyendo el for):
+ *
+ *      System.arraycopy(old, 0, lista, 0, old.length); o lista = Arrays.copyOf()
+ *      lista[old.length + 1] = p;
+ */
+        Profesor[] old = lista;
+
+        if (lista.length > 0)
+        {
+            int num = 0;
+
+            lista = new Profesor[old.length + 1];
+
+            for (Profesor pf : old)
+            {
+                lista[num] = pf;
+                num++;
+            }
+
+            lista[num] = p;
+        }
+
+        else
+        {
+            lista = new Profesor[1];
+
+            lista[0] = p;
+        }
+
+        return lista;
+    }
+
+    private static Profesor[] bajaProfesor(Profesor[] lista, int n)
+    {
+/*
+ * También se puede hacer de esta manera (sustituyendo el for):
+ *
+ *      if (n == 0)
+ *      {
+ *          System.arraycopy(old, 1, lista, 0, old.length);
+ *      }
+ *
+ *      else
+ *      {
+ *          System.arraycopy(old, n - 1, lista, 0, n);
+ *          System.arraycopy(old, n + 1, lista, n, old.length - n);
+ *      }
+ */
+        if (isValidIndex(lista, n) == "ok")
+        {
+            Profesor[] old = lista;
+            int num = 0;
+
+            lista = new Profesor[old.length - 1];
+
+            for (Profesor p : old)
+            {
+                if (num != n)
+                {
+                    lista[num > n ? num - 1 : num] = p;
+                }
+
+                num++;
+            }
+        }
+
+        return lista;
+    }
+
+    private static String isValidIndex(Profesor[] lista, int n)
+    {
+        String text;
+
+        if (lista.length > 0)
+        {
+            if (n > -1 && n <= lista.length)
+            {
+                text = "ok";
+            }
+
+            else
+            {
+                text = "No hay ningún profesor con ese número.";
+            }
+        }
+
+        else
+        {
+            text = "No hay ningún profesor creado.";
+        }
+
+        return text;
+    }
+
+    public static void main(String[] args)
+    {
+        Profesor[] lista = new Profesor[0];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Curso: ");
+        Profesor.setCurso(sc.nextLine());
+
+        System.out.print("\nImporte Horas Extra: ");
+        while (!sc.hasNextDouble()) sc.next();
+        Profesor.setPagoPorHoraExtra(sc.nextDouble());
+
+        int option = -1;
+
+        while (option != 0)
+        {
+            System.out.println("\n\n\tS E L E C C I O N E   U N A   O P C I O N\n");
+            System.out.println("\t1. ALTA DE UN PROFESOR");
+            System.out.println("\t2. BAJA DE UN PROFESOR");
+            System.out.println("\t3. CONSULTA DE DATOS PERSONALES DE UN PROFESOR");
+            System.out.println("\t4. INTRODUCIR HORAS EXTRAORDINARIAS DE UN MES");
+            System.out.println("\t5. LISTADO DE PROFESORES");
+            System.out.println("\t6. LISTADO DE NOMINAS DE UN MES");
+            System.out.println("\t0. SALIR DEL PROGRAMA");
+            System.out.print("\n\n\nOPCION SELECCIONADA: ");
+
+            if (sc.hasNextInt())
+            {
+                String text;
+                option = sc.nextInt();
+
+                switch (option)
+                {
+                    case 0: break;
+                    case 1: // new professor
+                    {
+                        Profesor p = new Profesor();
+
+                        p.nuevoProfesor(sc);
+
+                        lista = altaProfesor(lista, p);
+
+                        break;
+                    }
+                    case 2: // remove professor
+                    {
+                        System.out.print("Número del profesor a eliminar: ");
+                        while (!sc.hasNextInt()) sc.next();
+                        int num = sc.nextInt() - 1; // people normally hates 0, so easy fix
+
+                        text = isValidIndex(lista, num);
+
+                        if (text == "ok")
+                        {
+                            lista = bajaProfesor(lista, num);
+                            text = String.format("El profesor número %d ha sido eliminado.", num + 1); // consistent numbers
+                        }
+
+                        System.out.println(text);
+
+                        break;
+                    }
+                    case 3: // print professor data
+                    {
+                        System.out.print("Número del profesor a consultar: ");
+                        while (!sc.hasNextInt()) sc.next();
+                        int num = sc.nextInt() - 1; // people normally hates 0, so easy fix
+
+                        text = isValidIndex(lista, num);
+
+                        if (text == "ok")
+                        {
+                            text = lista[num].imprimeProfesor();
+                        }
+
+                        System.out.println(text);
+
+                        break;
+                    }
+                    case 4: // enter extra hours done by all professor for a given month
+                    {
+                        System.out.print("Horas extraordinarias realizadas por los profesores en el mes [1-12]: ");
+                        while (!sc.hasNextInt()) sc.next();
+                        int mes = sc.nextInt() - 1; // array index starts at 0
+
+                        if (mes > -1 && mes <= 11)
+                        {
+                            text = isValidIndex(lista, 0);
+
+                            if (text == "ok")
+                            {
+                                for (Profesor p : lista)
+                                {
+                                    System.out.printf("\nNombre profesor: %s\nHoras realizadas: ", p.getNombre());
+                                    while (!sc.hasNextInt()) sc.next();
+                                    int[] horasExtra = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                                    horasExtra[mes] = sc.nextInt();
+                                    p.setHorasExtra(horasExtra);
+                                }
+                            }
+
+                            else
+                            {
+                                System.out.println(text);
+                            }
+                        }
+
+                        else
+                        {
+                            System.out.println("El mes debe ser un número entre 1 y 12");
+                        }
+
+                        break;
+                    }
+                    case 5: // print every professor's data
+                    {
+                        int num = 1;
+
+                        text = isValidIndex(lista, 0);
+
+                        if (text == "ok")
+                        {
+                            text = "";
+
+                            for (Profesor p : lista)
+                            {
+                                text += String.format("\nNúmero profesor: %d\n%s\n", num, p.imprimeProfesor());
+
+                                num++;
+                            }
+                        }
+
+                        System.out.println(text);
+
+                        break;
+                    }
+                    case 6: // print all nominas (what are nominas lol) for a given month
+                    {
+                        System.out.print("Nóminas del mes: ");
+                        while (!sc.hasNextInt()) sc.next();
+                        int mes = sc.nextInt() - 1, // array index starts at 0
+                            num = 1;
+
+                        text = isValidIndex(lista, 0);
+
+                        if (text == "ok")
+                        {
+                            text = "";
+
+                            for (Profesor p : lista)
+                            {
+                                text += String.format("\nNúmero profesor: %d\n%s\n", num, p.imprimirNominas(mes));
+
+                                num++;
+                            }
+                        }
+
+                        System.out.println(text);
+
+                        break;
+                    }
+                    default:
+                    {
+                        System.out.println("La opcion debe ser un numero entre 0 y 6.");
+                    }
+                }
+            }
+
+            else
+            {
+                System.out.println("La opcion debe ser un numero entre 0 y 6.");
+                sc.next();
+            }
+        }
+
+        sc.close();
+    }
+}
---
+++ src/profesores/Profesor.java
@@ -0,0 +1,137 @@
+package profesores;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.time.Month;
+import java.time.format.TextStyle;
+
+public class Profesor
+{
+    private static String curso = "";
+    private static double pagoPorHoraExtra = 0;
+
+    private String dni = "", nombre = "";
+    private double sueldoBase = 0, tipoIRPF = 0;
+    private int[] horasExtra = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+    public void nuevoProfesor(Scanner sc)
+    {
+        sc.nextLine();
+
+        System.out.print("Profesor: ");
+        nombre = sc.nextLine();
+
+        System.out.print("DNI: ");
+        dni = sc.nextLine();
+
+        System.out.print("Sueldo Base: ");
+        while (!sc.hasNextDouble()) sc.next();
+        sueldoBase = sc.nextDouble();
+
+        System.out.print("Tipo de IRPF: ");
+        while (!sc.hasNextDouble()) sc.next();
+        tipoIRPF = sc.nextDouble();
+    }
+
+    private double calcularImporteHorasExtras(int mes)
+    {
+        return horasExtra[mes] * pagoPorHoraExtra;
+    }
+
+    private double calcularSueldoBruto(int mes)
+    {
+        return sueldoBase + calcularImporteHorasExtras(mes);
+    }
+
+    private double calcularRetencionesIrpf(int mes)
+    {
+        return (calcularSueldoBruto(mes) * tipoIRPF) / 100;
+    }
+
+    private double calcularSueldo(int mes)
+    {
+        return calcularSueldoBruto(mes) + calcularRetencionesIrpf(mes);
+    }
+
+    public String imprimeProfesor()
+    {
+        return String.format("Nombre: %s\nDNI: %s\nSueldo Base: %.2f\nTipo de IRPF: %.2f\n", nombre, dni, sueldoBase, tipoIRPF);
+    }
+
+    public String imprimirNominas(int mes)
+    {
+        return String.format("Nombre: %s\nDNI: %s\nCurso: %s   Nómina mes: %s\nSueldo Base: %.2f\nHoras Extra: %d\nTipo de IRPF: %.2f\nSueldo Bruto: %.2f\nRetención por IRPF: %.2f\nSueldo Neto: %.2f\n",
+                              nombre, dni, curso, Month.of(mes + 1).getDisplayName(TextStyle.FULL, Locale.getDefault()), sueldoBase,
+                              horasExtra[mes], tipoIRPF, calcularSueldoBruto(mes), calcularRetencionesIrpf(mes), calcularSueldo(mes));
+    }
+
+    public static String getCurso()
+    {
+        return curso;
+    }
+
+    public static void setCurso(String curso)
+    {
+        Profesor.curso = curso;
+    }
+
+    public static double getPagoPorHoraExtra()
+    {
+        return pagoPorHoraExtra;
+    }
+
+    public static void setPagoPorHoraExtra(double pagoPorHoraExtra)
+    {
+        Profesor.pagoPorHoraExtra = pagoPorHoraExtra;
+    }
+
+    public String getDni()
+    {
+        return dni;
+    }
+
+    public void setDni(String dni)
+    {
+        this.dni = dni;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public double getSueldoBase()
+    {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(double sueldoBase)
+    {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public double getTipoIRPF()
+    {
+        return tipoIRPF;
+    }
+
+    public void setTipoIRPF(double tipoIRPF)
+    {
+        this.tipoIRPF = tipoIRPF;
+    }
+
+    public int[] getHorasExtra()
+    {
+        return horasExtra;
+    }
+
+    public void setHorasExtra(int[] horasExtra)
+    {
+        this.horasExtra = horasExtra;
+    }
+}
